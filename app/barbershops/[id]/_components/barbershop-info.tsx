7526ea6a6bc7cdce9c1e57147d28accd0a8729a3
Cecíia Gama Souto <20201ireinfint0025@ifba.edu.br>
2024-04-09 16:12:45 -0300

    feat: add barbershop info component

---
+++ app/barbershops/[id]/_components/barbershop-info.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { Button } from "@/app/_components/ui/button";
+import { Barbershop } from "@prisma/client";
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+interface BarbershopInfoProps{
+    barbershop: Barbershop
+}
+
+const BarbershopInfo = ({barbershop}: BarbershopInfoProps) => {
+    const router = useRouter();
+    const handleBackClick = () =>{
+        router.back();
+    }
+    return ( <div>
+        <div className="h-[250px] w-full relative">
+            <Button onClick={handleBackClick} size="icon" variant="outline" className="z-50 absolute top-4 left-4">
+                <ChevronLeftIcon/>
+            </Button>
+
+            <Button size="icon" variant="outline" className="z-50 absolute top-4 right-4">
+                <MenuIcon/>
+            </Button>
+
+            <Image src={barbershop.imageURL}  
+            fill 
+            alt={barbershop.name}
+            style={{
+                objectFit:"cover",
+            }}
+            className="placeholder-opacity-75"
+            />
+        </div>
+
+            <div className="px-5 pt-3 pb-6 border-solid border-secondary">
+
+                <h1 className="text-xl font-bold">
+                    {barbershop.name}
+                </h1>
+
+                <div className="flex items-center gap-1 mt-2">
+                    <MapPinIcon className="stroke-primary" size={18}></MapPinIcon>
+                    <p className="text-sm">{barbershop.adress}</p>
+                </div>
+                
+                <div className="flex items-center gap-1 mt-2">
+                    <StarIcon className="stroke-primary" size={18}/>
+                    <p className="text-sm">5,0 (504 avaliações)</p>
+                </div>
+
+            </div>
+
+    </div> );
+}
+ 
+export default BarbershopInfo;
